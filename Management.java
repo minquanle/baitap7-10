91bd3a786ac13a209c460075c15198203a0fc787
minquanle <quanml39205@gmail.com>
2024-10-07 22:40:43 +0700

    update

--- Management.java
+++ Management.java
@@ -1,17 +1,20 @@
 public class Management {
     public static void main(String[] args) {
-        Animal pig = new Pig("Pig");
-        Animal duck = new Duck("Duck");
-        Animal fish = new Fish("Fish");
-        if(pig.move())
+        Animal Animal1 = new Pig("Animal1");
+        Animal Animal2 = new Duck("Animal2");
+        Animal Animal3 = new Fish("Animal3");
+        Animal[] animals = {Animal1, Animal2, Animal3};
+        for(Animal animal : animals)
         {
-            System.out.println("Pig can walk");
-        }
-        if(duck.move()) {
-            System.out.println("Duck can walk and swim");
-        }
-        if(fish.move()) {
-            System.out.println("Fish can swim");
+            if(animal instanceof Duck) {
+                System.out.println(animal.name + " swimmable and walkable");
+            }
+            else if(animal instanceof Swimming) {
+                System.out.println(animal.name + " swimmable");
+            }
+            else if(animal instanceof Walking) {
+                System.out.println(animal.name + " walkable");
+            }
         }
     }
 }
